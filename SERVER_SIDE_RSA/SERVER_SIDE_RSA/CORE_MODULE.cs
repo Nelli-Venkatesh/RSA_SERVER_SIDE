0e20494b174701adf81e63e67947a7bf2b0aeca0
Venkatesh-Nelli <venkatesh.nelli07@gmail.com>
2019-02-27 14:41:21 +0530

    Added Core Module Parameters

--- SERVER_SIDE_RSA/SERVER_SIDE_RSA/CORE_MODULE.cs
+++ SERVER_SIDE_RSA/SERVER_SIDE_RSA/CORE_MODULE.cs
@@ -8,5 +8,15 @@ namespace SERVER_SIDE_RSA
 {
     public class CORE_MODULE
     {
+        public string KEY_LENGTH { get; set; }
+        public string EXPONENT { get; set; }
+        public string MODULES { get; set; }
+        public string P { get; set; }
+        public string Q { get; set; }
+        public string D { get; set; }
+        public string DP { get; set; }
+        public string DQ { get; set; }
+        public string INVERSE_Q { get; set; }
+
     }
 }
