27e848b27f070dbaa1171540bb7afe7018df0f9e
Venkatesh-Nelli <venkatesh.nelli07@gmail.com>
2019-02-27 14:35:05 +0530

    First Commit with initila file creation

---
+++ SERVER_SIDE_RSA/SERVER_SIDE_RSA/AES_MODULE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVER_SIDE_RSA
+{
+    class AES_MODULE
+    {
+    }
+}
---
+++ SERVER_SIDE_RSA/SERVER_SIDE_RSA/CORE_MODULE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVER_SIDE_RSA
+{
+    public class CORE_MODULE
+    {
+    }
+}
